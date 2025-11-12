777287945212d771d72017ef833a78499881c25c
filix <florindozec@gmail.com>
2025-11-12 20:00:59 +0100

    add gitingore

--- data_analysis/Main.py
+++ data_analysis/Main.py
@@ -5,7 +5,7 @@ from utils.CurveDetector import CurveDetector
 def main():
     """Funzione main del progetto Iot Zecconi e Gagliotti"""
     print("Ciao dal main!")
-    curve_detector = CurveDetector("4_tel.json", "corners_M.json")
+    curve_detector = CurveDetector("data/4_tel.json", "data/corners_M.json")
     curves = curve_detector.calcolo_curve()
     for i in range(0 , len(curves)):
         curves[i].print();
