ed5dde0f0f73a49d08b4f15886a29dd56d37361c
filix <florindozec@gmail.com>
2025-11-19 17:20:05 +0100

    creaziole algoritmo di ricomoscimento e grafici rifatti

--- data_analysis/Main.py
+++ data_analysis/Main.py
@@ -5,7 +5,7 @@ from utils.CurveDetector import CurveDetector
 def main():
     """Funzione main del progetto Iot Zecconi e Gagliotti"""
     print("Ciao dal main!")
-    curve_detector = CurveDetector("data/1_telS_OUTLAP.json", "data/corners_S.json")
+    curve_detector = CurveDetector("data/9_telB.json", "data/corners_B.json")
     curves = curve_detector.calcolo_curve()
     for i in range(0 , len(curves)):
        
@@ -18,7 +18,7 @@ def main():
         # curves[i].plot_aggressiveness_profile(use_time=True)
         # curves[i].plot_fluidity_profile()
         # curves[i].plot_trail_braking_profile(use_time=True)
-        # curves[i].plot_grip_usage_profile(use_time=True)
+        curves[i].plot_grip_usage_profile(use_time=True)
         print(Curve.classify_driver_style(curves[i]))
         
     curve_detector.grafico(curves,False)
