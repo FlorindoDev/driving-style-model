eb3777da2be0e7539b3d51b05a11772cf6b35d2f
Florindo Zecconi <florindozec@gmail.com>
2026-01-26 15:27:08 +0100

    fix

--- data_analysis/Main.py
+++ data_analysis/Main.py
@@ -5,11 +5,7 @@ from utils.CurveDetector import CurveDetector
 def main():
     """Funzione main del progetto Iot Zecconi e Gagliotti"""
     print("Ciao dal main!")
-<<<<<<< HEAD
     curve_detector = CurveDetector("data/48_telL_SLOW.json", "data/corners_L.json")
-=======
-    curve_detector = CurveDetector("data/21_telS_OUTLAP.json", "data/corners_S.json")
->>>>>>> bb2f3673385c3c01a5e8f7fa77fcc8eb3cab3334
     curves = curve_detector.calcolo_curve()
     #for i in range(0 , len(curves)):
        
@@ -23,11 +19,7 @@ def main():
         # curves[i].plot_fluidity_profile()
         # curves[i].plot_trail_braking_profile(use_time=True)
         # curves[i].plot_grip_usage_profile(use_time=True)
-<<<<<<< HEAD
         #print(Curve.classify_driver_style(curves[i]))
-=======
-        print(Curve.classify_driver_style(curves[i]))
->>>>>>> bb2f3673385c3c01a5e8f7fa77fcc8eb3cab3334
         
     curve_detector.grafico(curves,False)
     curve_detector.plot_curve_trajectories(curves)
