4de9114ae11204cb1880ae6c0b64fc38ed4aee98
filix <florindozec@gmail.com>
2026-01-29 17:55:00 +0100

    gitignore

--- neural_model/main.py
+++ neural_model/main.py
@@ -1,13 +1,2 @@
 import time
 import torch
-
-print("torch:", torch.__version__)
-print("torch.version.hip:", torch.version.hip)
-print("cuda.is_available (ROCm usa questa API):", torch.cuda.is_available())
-
-if torch.cuda.is_available():
-    print("device count:", torch.cuda.device_count())
-    print("device 0:", torch.cuda.get_device_name(0))
-    device = torch.device("cuda:0")
-else:
-    device = torch.device("cpu")
